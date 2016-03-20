8f2ecf390d28576d554f4d65e8494f053e7514c6
Wuyan Xu <Wuyan Xu>
2016-03-20 14:04:00 +0100

    Auto-zoom to last 2 hours on page load, also added buttons for zooming.

--- www/g.php
+++ www/g.php
@@ -16,6 +16,10 @@
     
     <p>
       <input type="button" value="Unzoom" onclick="unzoomGraph()">&nbsp;
+      <input type="button" value="Last 8 hours" onclick="selectLasthours(8)">&nbsp;
+      <input type="button" value="Last 4 hours" onclick="selectLasthours(4)">&nbsp;
+      <input type="button" value="Last 2 hours" onclick="selectLasthours(2)">&nbsp;
+      <input type="button" value="Last hour" onclick="selectLasthours(1)">&nbsp;
     </p>
     <p><b>Select Data to show:</b><br/>
       <input type=checkbox id="0" checked onClick="change(this)">
@@ -28,6 +32,12 @@
 
     
     <script type="text/javascript">
+        // On page load, run: 
+        window.onload = function(){
+           setTimeout(function() { selectLasthours(2); }, 100);
+        };
+        
+        // Drawing the graph 
         g = new Dygraph(
         document.getElementById("mygraph"),
         "log/solar.csv", 
@@ -36,7 +46,7 @@
             rollPeriod: 4,
             zoomCallback : function(minDate, maxDate, yRange) {
                         showDimensions(minDate, maxDate, yRange);
-              },
+              }
             }
         );
         
@@ -50,13 +60,25 @@
         setStatus();
         }
         
-        // For unzoom button 
+        // For zoom buttons 
         function unzoomGraph() {
         g.updateOptions({
           dateWindow: null,
           valueRange: null
         });
         }
+        function selectLasthours(hours) {
+        var range = g.xAxisRange();
+        var maxX = range[1];
+        var minX = maxX - hours * 60 * 60 * 1000;
+        g.updateOptions(
+        {
+            dateWindow: [minX, maxX],
+        });
+        }
+        
+        
+        
     </script>
 
 </body>
