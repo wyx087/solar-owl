4ab7033ee99cbd527c83f7287a6a1c622ccb8b66
Wuyan Xu <Wuyan Xu>
2016-03-20 11:31:27 +0100

    Graph had data series selector added.

--- www/g.php
+++ www/g.php
@@ -5,22 +5,41 @@
   src="dygraph-combined.js"></script>
 </head>
 <body>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<br><br>
-
-<div id="mygraph"
-  style="width:800px; height:600px;"></div>
-<script type="text/javascript">
-  g2 = new Dygraph(
-    document.getElementById("mygraph"),
-    "log/solar.csv", 
-    {
-	labels: [ "Time", "Usage", "Generating", "Exporting" ],
-    rollPeriod: 16
-    }
-  );
-</script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <br>
+    
+    <div id="mygraph"
+        style="width:800px; height:600px;"></div>
+    
+    <p><b>Select Data to show:</b></p>
+    <p>
+      <input type=checkbox id="0" checked onClick="change(this)">
+      <label for="0"> Usage</label><br/>
+      <input type=checkbox id="1" checked onClick="change(this)">
+      <label for="1"> Generating</label><br/>
+      <input type=checkbox id="2" checked onClick="change(this)">
+      <label for="2"> Exporting</label>
+    </p>
+    
+    <script type="text/javascript">
+        g = new Dygraph(
+        document.getElementById("mygraph"),
+        "log/solar.csv", 
+            {
+            labels: [ "Time", "Usage", "Generating", "Exporting" ],
+            rollPeriod: 16
+            }
+        );
+        
+        function setStatus() {
+        document.getElementById("visibility").innerHTML =
+          g.visibility().toString();
+        }
+        function change(el) {
+        g.setVisibility(parseInt(el.id), el.checked);
+        setStatus();
+        }
+    </script>
 
 </body>
 </html>
