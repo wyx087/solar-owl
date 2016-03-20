eea85ce60ffaa9cde5299c001447655808655c9a
Wuyan Xu <Wuyan Xu>
2016-03-20 12:30:43 +0100

    Added Unzoom button, in prep for default zoomed view.

--- www/g.php
+++ www/g.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7; IE=EmulateIE9">
 <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 <meta content="utf-8" http-equiv="encoding">
 <script type="text/javascript"
@@ -13,8 +14,10 @@
     <div id="mygraph"
         style="width:800px; height:600px;"></div>
     
-    <p><b>Select Data to show:</b></p>
     <p>
+      <input type="button" value="Unzoom" onclick="unzoomGraph()">&nbsp;
+    </p>
+    <p><b>Select Data to show:</b><br/>
       <input type=checkbox id="0" checked onClick="change(this)">
       <label for="0"> Usage</label><br/>
       <input type=checkbox id="1" checked onClick="change(this)">
@@ -22,6 +25,7 @@
       <input type=checkbox id="2" checked onClick="change(this)">
       <label for="2"> Exporting</label>
     </p>
+
     
     <script type="text/javascript">
         g = new Dygraph(
@@ -29,10 +33,14 @@
         "log/solar.csv", 
             {
             labels: [ "Time", "Usage", "Generating", "Exporting" ],
-            rollPeriod: 16
+            rollPeriod: 4,
+            zoomCallback : function(minDate, maxDate, yRange) {
+                        showDimensions(minDate, maxDate, yRange);
+              },
             }
         );
         
+        // For Data selector 
         function setStatus() {
         document.getElementById("visibility").innerHTML =
           g.visibility().toString();
@@ -40,6 +48,14 @@
         function change(el) {
         g.setVisibility(parseInt(el.id), el.checked);
         setStatus();
+        }
+        
+        // For unzoom button 
+        function unzoomGraph() {
+        g.updateOptions({
+          dateWindow: null,
+          valueRange: null
+        });
         }
     </script>
 
