8570ea1921a70b0647607eb0d446d80ee5ab5cca
Wuyan Xu <Wuyan Xu>
2016-03-20 20:26:36 +0100

    Still WIP on graph update, got graph drawing into a function. Also tidied up data selection.

--- www/g.php
+++ www/g.php
@@ -32,6 +32,14 @@
     <div id="mygraph"
         style="width:800px; height:600px;"></div>
     
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <input type=checkbox id="0" checked onClick="change(this)">
+      <label for="0"> <font color="#000000">Usage</font></label>&nbsp;
+      <input type=checkbox id="1" checked onClick="change(this)">
+      <label for="1"> <font color="#00E000">Generating</font></label>&nbsp;
+      <input type=checkbox id="2" checked onClick="change(this)">
+      <label for="2"> <font color="#FF0000">Exporting</font></label>&nbsp;
+    </p>
     <p>
       <input type="button" value="Unzoom" onclick="unzoomGraph()">&nbsp;
       <input type="button" value="Last 8 hours" onclick="selectLasthours(8)">&nbsp;
@@ -39,32 +47,28 @@
       <input type="button" value="Last 2 hours" onclick="selectLasthours(2)">&nbsp;
       <input type="button" value="Last hour" onclick="selectLasthours(1)">&nbsp;
     </p>
-    <p><b>Select Data to show:</b>
-      <input type=checkbox id="0" checked onClick="change(this)">
-      <label for="0"> <font color="#000000">Usage</font></label>
-      <input type=checkbox id="1" checked onClick="change(this)">
-      <label for="1"> <font color="#00E000">Generating</font></label>
-      <input type=checkbox id="2" checked onClick="change(this)">
-      <label for="2"> <font color="#FF0000">Exporting</font></label>
-    </p>
+
 
     
     <script type="text/javascript">
         // On page load, run: 
         window.onload = function(){
-           setTimeout(function() { selectLasthours(2); }, 500);
+            setTimeout(function() { drawgraph(); }, 10);
+            setTimeout(function() { selectLasthours(2); }, 500);
         };
         
         // Drawing the graph 
-        g = new Dygraph(
-        document.getElementById("mygraph"),
-        "log/solar.csv", 
-            {
-                labels: [ "Time", "Usage", "Generating", "Exporting" ],
-                colors: ['#000000', '#00E000', '#FF0000'],
-                rollPeriod: 2
-            }
-        );
+        function drawgraph() {
+            g = new Dygraph(
+            document.getElementById("mygraph"),
+            "log/solar.csv", 
+                {
+                    labels: [ "Time", "Usage", "Generating", "Exporting" ],
+                    colors: ['#000000', '#00E000', '#FF0000'],
+                    rollPeriod: 2
+                }
+            );
+        };
         
         // For Data selector 
         function setStatus() {
@@ -91,7 +95,6 @@
             dateWindow: [minX, maxX],
         });
         }
-        
         
         
     </script>
