c7068b77b648b380a3264a76c555be1205ec73a1
wyx087 <wuyanxu@hotmail.com>
2019-11-03 21:55:32 +0000

    As per previous commit, also the scripts.

---
+++ logsave.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#   Log file trim script 
+
+# file=/var/tmp/solar.log
+# minimumsize=200000
+# actualsize=$(wc -c <"$file")
+# if [ $actualsize -ge $minimumsize ]; then
+#   echo $file  size is $actualsize bytes over $minimumsize bytes, renaming
+#   mv $file /var/tmp/solar-1.log
+# fi
+
+
+#   Smooth log file archive to disk  
+
+file=/var/tmp/solar_graph.log
+DATE=$(date +%Y-%m-%d)
+mv $file /home/pi/solar/log/solar_$DATE.csv
