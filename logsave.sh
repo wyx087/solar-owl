925860deefca888e78863acb8874938b2f48be3a
wyx087 <wuyanxu@hotmail.com>
2019-11-06 09:05:33 +0000

    Changed logsave to append rather than overwrite.

--- logsave.sh
+++ logsave.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-#   Log file trim script 
-
-# file=/var/tmp/solar.log
-# minimumsize=200000
-# actualsize=$(wc -c <"$file")
-# if [ $actualsize -ge $minimumsize ]; then
-#   echo $file  size is $actualsize bytes over $minimumsize bytes, renaming
-#   mv $file /var/tmp/solar-1.log
-# fi
-
-
-#   Smooth log file archive to disk  
+#   Save CSV log file archive to disk  
 
 file=/var/ramdisk/solar_graph.log
 DATE=$(date +%Y-%m-%d)
-cp $file /home/pi/solar/log/solar_$DATE.csv
+cat $file >> /home/pi/solar/log/solar_$DATE.csv
